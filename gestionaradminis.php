e4e34cb1f4ae5ec74ec23946e51037da60286993
ByronCarrion <carrionmejia756@gmail.com>
2019-04-23 01:44:54 -0600

    Agregar tablas

--- gestionaradminis.php
+++ gestionaradminis.php
@@ -3,12 +3,33 @@ include "Header.php";
 include "Meta.php";
 
  ?>
+<br>
 
+ <section class="bs-docs-section container">
+    <article class="row table-responsive">
 
- <section>
-
-  
- </section>
+            <table class="table table-bordered table-hover " >
+        <thead class="thead-dark">
+            <tr class="table-success">
+            <th scope="col">Nombres y Apellidos</th>
+            <th scope="col">Usuario</th>
+            <th scope="col">Tipo de Usuario</th>
+            <th scope="col">Estado</th>
+            <th scope="col">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+                <tr>
+                <td>Fanor Rivera</td>
+                <td>frivera</td>
+                <td>Administrador</td>
+                <td>Activo</td>
+                <td><a href="#"><i class="fa fa-trash"></i></a> &nbsp &nbsp <a href="#"><i class="fa fa-pen-alt"></i></a></td>
+                </tr>
+        </tbody>
+        </table>
+    </article>
+</section>
 
  <<?php
  include "PiePagina.php";
